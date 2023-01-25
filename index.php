0b1f95220eca2c9fffe6b394f96a9206fdf4fcbe
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-25 16:39:33 +0100

    Add hotel list to index.php file

--- index.php
+++ index.php
@@ -1,18 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Hotel</title>
-    </head>
-    <body>
-        <header>
-            Hotel List
-        </header>
 
-        <main>
-            
-        </main>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+
+<body>
+    <header>
+        Hotel List
+    </header>
+
+    <main>
+        <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+
+        foreach ($hotels as $hotelEl){
+            // foreach ($hotelEl as $key => $value){
+            //     echo "<h2> {$value} </h2>";
+            // }
+            echo "<h2> $hotelEl[name] </h2>";
+            echo "<p> $hotelEl[description] </p>";
+            echo "<h4> Voto: $hotelEl[vote] </h4>";
+            echo "<h4> Distanza dal centro: $hotelEl[distance_to_center] </h4>";
+        }
+        ?>
+    </main>
+</body>
+
 </html>
